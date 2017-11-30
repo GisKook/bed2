bf30c1bc133d252f0248018e74ce2986f614646c
zhangkai <zhangkai.gis@163.com>
2017-11-30 13:48:08 +0800

    rm  log

--- socket_server/socket_server_protocol.go
+++ socket_server/socket_server_protocol.go
@@ -3,7 +3,7 @@ package socket_server
 import (
 	"errors"
 	"github.com/gansidui/gotcp"
-	"log"
+	//"log"
 	"net"
 )
 
@@ -23,15 +23,15 @@ func (ss *SocketServer) ReadPacket(conn *net.TCPConn) (gotcp.Packet, error) {
 	data := make([]byte, 1024)
 	length, err := conn.Read(data)
 	if err != nil {
-		log.Printf("<READ>%x  %s\n", conn, err.Error())
+		//	log.Printf("<READ>%x  %s\n", conn, err.Error())
 		return nil, err
 	}
 
 	if length == 0 {
-		log.Println(ErrPeerClosed.Error())
+		//	log.Println(ErrPeerClosed.Error())
 		return nil, ErrPeerClosed
 	}
-	log.Printf("<IN>  %x  %x\n", conn, data[0:length])
+	//log.Printf("<IN>  %x  %x\n", conn, data[0:length])
 
 	return &Raw{
 		raw: data[0:length],
