aa8d24385c1cae94ab687908fb65c3b0e8e67a62
zhangkai <zhangkai.gis@163.com>
2017-11-30 14:37:04 +0800

    del log

--- main/main.go
+++ main/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"flag"
+	//"flag"
 	"fmt"
 	"github.com/giskook/bed2/conf"
 	"github.com/giskook/bed2/reactor"
@@ -9,25 +9,25 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"runtime/pprof"
+	//"runtime/pprof"
 	"syscall"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+//var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
+//var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
+	//	flag.Parse()
+	//	if *cpuprofile != "" {
+	//		f, err := os.Create(*cpuprofile)
+	//		if err != nil {
+	//			log.Fatal("could not create CPU profile: ", err)
+	//		}
+	//		if err := pprof.StartCPUProfile(f); err != nil {
+	//			log.Fatal("could not start CPU profile: ", err)
+	//		}
+	//		defer pprof.StopCPUProfile()
+	//	}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	// read configuration
@@ -42,17 +42,17 @@ func main() {
 	signal.Notify(chSig, syscall.SIGINT, syscall.SIGTERM)
 	fmt.Println("Signal: ", <-chSig)
 
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-		f.Close()
-	}
+	//	if *memprofile != "" {
+	//		f, err := os.Create(*memprofile)
+	//		if err != nil {
+	//			log.Fatal("could not create memory profile: ", err)
+	//		}
+	//		runtime.GC() // get up-to-date statistics
+	//		if err := pprof.WriteHeapProfile(f); err != nil {
+	//			log.Fatal("could not write memory profile: ", err)
+	//		}
+	//		f.Close()
+	//	}
 	// stops service
 	reactor.Close()
 }
